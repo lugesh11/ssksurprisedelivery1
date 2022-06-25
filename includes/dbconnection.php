a607ab6e151f3f098367c188b0e72a3675911f08
lugesh11 <108191998+lugesh11@users.noreply.github.com>
2022-06-26 04:27:35 +0800

    updation

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "ssk");
+$con=mysqli_connect("remotemysql.com", "3HTF6ybyk5", "6kYUQQnM3F", "3HTF6ybyk5");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
